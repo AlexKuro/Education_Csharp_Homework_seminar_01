6e6c718d684c70c1f44c0f009e45e8eaa3d0cd1c
AlexKuro <89215682813@mail.ru>
2022-11-12 23:49:24 +0300

    Seminar_01_Task_08

---
+++ Homework/Task_08/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 8: Напишите программу, которая на вход принимает число (N > 0), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8*/
+
+int number;
+Console.WriteLine("|------Задача 8-------|");
+
+Console.Write("Введите число:-> ");
+number = Convert.ToInt32(Console.ReadLine());
+
+PrintPower(number);
+
+void PrintPower(int number_1)
+{
+    if (number_1 > 0)
+    {  
+        int count = 1;
+        while (count <= number_1)
+        {
+            if (count % 2 == 0)
+            {
+                Console.Write($"{count} ");                   
+            }
+        count ++;
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Число {number_1} отрицательное, введите число > 0!");
+    }
+}
